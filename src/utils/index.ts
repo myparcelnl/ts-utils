55b4be0f411331d04d38d1504221d2f4b1ac1188
Edie Lemoine <edie@myparcel.nl>
2023-02-06 13:03:37 +0100

    fix: update index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,6 @@
 export * from './asyncEvery';
 export * from './asyncSome';
+export * from './isUndefined';
 export * from './partitionArray';
+export * from './removePropertiesWith';
 export * from './toArray';

f1a68cbf595783620f33a036584cdfc482989bb7
Edie Lemoine <edie@myparcel.nl>
2023-02-08 09:19:31 +0100

    feat(type-guards): make isOfType work on inherited object properties

--- src/__tests__/type-guards/isOfType.spec.ts
+++ src/__tests__/type-guards/isOfType.spec.ts
@@ -6,15 +6,61 @@ type MyObjectType = {
 };
 
 describe('isOfType', () => {
-  it('checks for value', () => {
+  it('works on objects', () => {
     expect(isOfType<MyObjectType>({ref: 'foo'}, 'ref')).toBe(true);
   });
 
-  it('does not match undefined', () => {
-    expect(isOfType<MyObjectType>({ref: undefined}, 'ref')).toBe(false);
+  it('works on objects with inherited properties', () => {
+    expect(isOfType<MyObjectType>(Object.create({ref: 'foo'}), 'ref')).toBe(true);
   });
 
-  it('does not match wrong keys', () => {
-    expect(isOfType<MyObjectType>('string', 'ref')).toBe(false);
+  it('works on strings', () => {
+    expect(isOfType<string>('', 'substring')).toBe(true);
+  });
+
+  it('works on arrays', () => {
+    expect(isOfType<string[]>(['string'], 'length')).toBe(true);
+  });
+
+  it('works on functions', () => {
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    expect(isOfType<Function>(() => undefined, 'call')).toBe(true);
+  });
+
+  it('works on classes', () => {
+    class MyClass {
+      public foo = 'foo';
+    }
+
+    expect(isOfType<MyClass>(new MyClass(), 'foo')).toBe(true);
+  });
+
+  it('works on classes with inherited properties', () => {
+    class MyClass {
+      public foo = 'foo';
+    }
+
+    class ChildClass extends MyClass {}
+
+    expect(isOfType<MyClass>(new ChildClass(), 'foo')).toBe(true);
+  });
+
+  it.each([
+    [undefined],
+    [null],
+    [0],
+    [1],
+    [true],
+    [false],
+    [NaN],
+    [Infinity],
+    [Symbol('foo')],
+    [(): void => undefined],
+    [new Date()],
+    [new Error()],
+    [new Map()],
+    [new Set()],
+  ])('does not match on %s', (value) => {
+    expect(isOfType<MyObjectType>(value, 'ref')).toBe(false);
   });
 });
--- src/type-guards/isOfType.ts
+++ src/type-guards/isOfType.ts
@@ -1,8 +1,8 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-explicit-any */
 
 /**
- * Type guard for checking if an object value is of a specific type by checking if a given K exists.
+ * Type guard for checking if an object value is of a specific type by checking if a given property exists.
  */
 export function isOfType<T>(value: any, property: keyof T): value is T {
-  return value?.hasOwnProperty(property) && undefined !== value[property];
+  return value?.[property] !== undefined;
 }

30a5de2f48139a16871553cc91195f58434e8fa2
Edie Lemoine <edie@myparcel.nl>
2023-03-03 12:03:07 +0100

    feat(type-guards): support other value types in isEnumValue

--- src/__tests__/type-guards/isEnumValue.spec.ts
+++ src/__tests__/type-guards/isEnumValue.spec.ts
@@ -6,6 +6,11 @@ enum MyEnum {
   JOHNSON = 'johnson',
 }
 
+enum MyNumberEnum {
+  DUANE = 4880,
+  JOHNSON = 4881,
+}
+
 describe('isEnumValue', () => {
   it('returns true if value is present in enum', () => {
     expect(isEnumValue('duane', MyEnum)).toBe(true);
@@ -13,5 +18,13 @@ describe('isEnumValue', () => {
 
   it('returns false if value is not present in enum', () => {
     expect(isEnumValue('book', MyEnum)).toBe(false);
+  });
+
+  it('returns true if value is present in number enum', () => {
+    expect(isEnumValue(4880, MyNumberEnum)).toBe(true);
+  });
+
+  it('returns false if value is not present in number enum', () => {
+    expect(isEnumValue(1, MyNumberEnum)).toBe(false);
   });
 });
